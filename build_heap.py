f8dbe956d9b4d48c9ca7978570c7e13323db6963
Elīza Kanska 221RDB095 <78021756+elizakanska@users.noreply.github.com>
2023-03-19 17:09:59 +0200

    Update build_heap.py

--- build_heap.py
+++ build_heap.py
@@ -1,12 +1,34 @@
 # python3
+# Elīza Kanska 221RDB095
 
 
 def build_heap(data):
     swaps = []
-    # TODO: Creat heap and heap sort
+    # TODO: Create heap and heap sort
     # try to achieve  O(n) and not O(n2)
+    n = len(data)
 
-
+    def down(j):
+        nonlocal swaps
+        min = j
+        right_child = 2 * j + 2
+        left_child = 2 * j + 1
+        
+        if data[right_child] < data[min] and right_child < n:
+            min = right_child
+        if data[left_child] < data[min] and left_child < n:
+            min = left_child
+        
+        if min != j:
+            data[j], data[min] = data[min], data[j]
+            swaps.append((j, min))
+            down(min)
+            
+    not_a_leaf = (n - 2) // 2
+    
+    for j in range(not_a_leaf, -1, -1):
+        down(j)
+        
     return swaps
 
 
@@ -15,11 +37,18 @@ def main():
     # TODO : add input and corresponding checks
     # add another input for I or F 
     # first two tests are from keyboard, third test is from a file
-
+    n = input().strip()
 
     # input from keyboard
-    n = int(input())
-    data = list(map(int, input().split()))
+    if n == 'I':
+        n = int(input())
+        data = list(map(int, input().split()))
+        
+    elif n == 'F':
+        file = input()
+        with open("tests/" + file, 'r') as fin:
+            n = int(fin.readline().strip())
+            data = list(map(int, fin.readline().split()))
 
     # checks if lenght of data is the same as the said lenght
     assert len(data) == n
